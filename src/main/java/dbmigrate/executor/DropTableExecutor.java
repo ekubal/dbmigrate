d8ea03de4939f0381a747ec4e7359584767d6985
Piotr Krawiec <pkrawiec@student.agh.edu.pl>
2011-10-27 18:20:40 +0200

    Added drop table executor stub.

---
+++ src/main/java/dbmigrate/executor/DropTableExecutor.java
@@ -0,0 +1,10 @@
+package dbmigrate.executor;
+
+public class DropTableExecutor implements IExecutor<DropTableOperationDescriptor> {
+
+	@Override
+	public void execute(DropTableOperationDescriptor operation) {
+		
+	}
+
+}
