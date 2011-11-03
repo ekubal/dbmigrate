88f1fb438d98c135cca0e0534e9bf724003d9e92
zyxist <nightstorm@tlen.pl>
2011-11-03 17:46:10 +0100

    Removing unecessary @Override annotation.

--- src/main/java/dbmigrate/executor/DropTableExecutor.java
+++ src/main/java/dbmigrate/executor/DropTableExecutor.java
@@ -3,7 +3,6 @@ package dbmigrate.executor;
 import dbmigrate.model.operation.DropTableOperationDescriptor;
 
 public class DropTableExecutor implements IExecutor<DropTableOperationDescriptor> {
-	@Override
 	public void execute(DropTableOperationDescriptor operation) {
 		
 	}
