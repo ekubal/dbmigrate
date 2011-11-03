d49e1e8706a01d84a0bb8da2f5d53d7bf760ae2e
Piotr Krawiec <pkrawiec@student.agh.edu.pl>
2011-11-03 17:40:46 +0100

    Extracted column interface.

--- src/main/java/dbmigrate/model/db/IColumn.java
+++ src/main/java/dbmigrate/model/db/IColumn.java
@@ -1,6 +1,27 @@
 package dbmigrate.model.db;
 
 public interface IColumn {
-	// TODO: write
-}
 
+	public abstract String getSqlDescription();
+
+	public abstract String getName();
+
+	public abstract void setName(String name);
+
+	public abstract TypeEnum getType();
+
+	public abstract void setType(TypeEnum type);
+
+	public abstract Boolean getNullable();
+
+	public abstract void setNullable(Boolean nullable);
+
+	public abstract Boolean getSigned();
+
+	public abstract void setSigned(Boolean signed);
+
+	public abstract int getLength();
+
+	public abstract void setLength(int length);
+
+}
