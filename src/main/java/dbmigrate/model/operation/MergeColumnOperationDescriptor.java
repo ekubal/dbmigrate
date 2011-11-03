537d44f252efa8ca7a71d5a3f79d7a1d8e66a238
Jakub Lasek <ekubal1908@gmail.com>
2011-11-03 22:44:18 +0100

    Split and merge operations descriptors refs #60

---
+++ src/main/java/dbmigrate/model/operation/MergeColumnOperationDescriptor.java
@@ -0,0 +1,65 @@
+package dbmigrate.model.operation;
+
+import dbmigrate.model.db.Column;
+
+public class MergeColumnOperationDescriptor implements IOperationDescriptor {
+	private Column sourceColumn1;
+	private Column sourceColumn2;
+	private String tableName;
+	private String delimiter;
+	private AddColumnOperationDescriptor destinationColumn;
+
+	public MergeColumnOperationDescriptor(Column sourceColumn1,
+			Column sourceColumn2, String tableName, String delimiter,
+			AddColumnOperationDescriptor destinationColumn) {
+		super();
+		this.sourceColumn1 = sourceColumn1;
+		this.sourceColumn2 = sourceColumn2;
+		this.tableName = tableName;
+		this.delimiter = delimiter;
+		this.destinationColumn = destinationColumn;
+	}
+
+	public Column getSourceColumn1() {
+		return sourceColumn1;
+	}
+
+	public void setSourceColumn1(Column sourceColumn1) {
+		this.sourceColumn1 = sourceColumn1;
+	}
+
+	public Column getSourceColumn2() {
+		return sourceColumn2;
+	}
+
+	public void setSourceColumn2(Column sourceColumn2) {
+		this.sourceColumn2 = sourceColumn2;
+	}
+
+	public String getTableName() {
+		return tableName;
+	}
+
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+
+	public String getDelimiter() {
+		return delimiter;
+	}
+
+	public void setDelimiter(String delimiter) {
+		this.delimiter = delimiter;
+	}
+
+	public AddColumnOperationDescriptor getDestinationColumn() {
+		return destinationColumn;
+	}
+
+	public void setDestinationColumn(
+			AddColumnOperationDescriptor destinationColumn) {
+		this.destinationColumn = destinationColumn;
+	}
+
+	
+}
