f5b6d3564c8fdf741384255bc391a982a0efb3db
Magda Lesniak <maglesniak0@gmail.com>
2011-11-03 12:10:32 +0100

    Database connector added

---
+++ src/main/java/dbmigrate/model/db/DbConnector.java
@@ -0,0 +1,25 @@
+package dbmigrate.model.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnector {
+	
+	  public Connection getConnection(String databaseType, String host,String dbName, String user, String password){
+			  
+		  Connection connection = null;
+		  if(databaseType=="postgresql"){
+			  try {
+				    connection = DriverManager.getConnection("jdbc:postgresql://"+host+"/"+dbName,
+				                                    user, password);
+				  } catch (SQLException se) {
+				    System.out.println("Couldn't connect.");
+				    se.printStackTrace();
+				    System.exit(1);
+				  }
+		  }
+		  return connection;
+	  }
+
+}
