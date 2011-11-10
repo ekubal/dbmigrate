b55c67c384cf06486dc3466018cc05799b20933b
Jakub Lasek <ekubal1908@gmail.com>
2011-11-10 18:29:42 +0100

    migrations

--- src/main/java/dbmigrate/executor/MergeColumnExecutor.java
+++ src/main/java/dbmigrate/executor/MergeColumnExecutor.java
@@ -1,8 +1,10 @@
 package dbmigrate.executor;
 
+import dbmigrate.model.operation.DropColumnOperationDescriptor;
 import dbmigrate.model.operation.MergeColumnOperationDescriptor;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MergeColumnExecutor extends GeneralExecutor<MergeColumnOperationDescriptor> {
 	public MergeColumnExecutor(Connection connection) {
@@ -10,17 +12,21 @@ public class MergeColumnExecutor extends GeneralExecutor<MergeColumnOperationDes
 	}
 	
 	public void execute(MergeColumnOperationDescriptor operation) throws SQLException {
+		(new AddColumnExecutor(this.getConnection())).execute(operation.getDestinationColumn());
+		
 		StringBuffer buf = new StringBuffer();
 		buf.append("UPDATE \"").append(operation.getTableName()).append("\" SET ")
 				.append(operation.getDestinationColumn().getTableName())
 				.append(" = ").append(operation.getSourceColumn1())
 				.append(" || '").append(operation.getDelimiter())
 				.append("' || ").append(operation.getSourceColumn2());
-		(new AddColumnExecutor(this.getConnection())).execute(operation.getDestinationColumn());
 		
-		//connector.query
+		Statement stmt = getConnection().createStatement();
+        stmt.executeUpdate(buf.toString());
 		
-		// drop column
+        DropColumnExecutor dcx = new DropColumnExecutor(getConnection());
+        DropColumnOperationDescriptor dcod = new DropColumnOperationDescriptor(operation.getTableName(), operation.getSourceColumn1());
+        dcx.execute(dcod);
 	}
 
 }
