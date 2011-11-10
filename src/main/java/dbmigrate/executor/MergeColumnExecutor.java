a85ab5537d0c834034488341323d358421f2b89d
Jakub Lasek <ekubal1908@gmail.com>
2011-11-10 18:34:46 +0100

    merge column executor

--- src/main/java/dbmigrate/executor/MergeColumnExecutor.java
+++ src/main/java/dbmigrate/executor/MergeColumnExecutor.java
@@ -1,32 +1,42 @@
 package dbmigrate.executor;
 
+import dbmigrate.model.db.ITable;
+import dbmigrate.model.db.Table;
 import dbmigrate.model.operation.DropColumnOperationDescriptor;
 import dbmigrate.model.operation.MergeColumnOperationDescriptor;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class MergeColumnExecutor extends GeneralExecutor<MergeColumnOperationDescriptor> {
+public class MergeColumnExecutor extends
+		GeneralExecutor<MergeColumnOperationDescriptor> {
 	public MergeColumnExecutor(Connection connection) {
 		this.setConnection(connection);
 	}
-	
-	public void execute(MergeColumnOperationDescriptor operation) throws SQLException {
-		(new AddColumnExecutor(this.getConnection())).execute(operation.getDestinationColumn());
-		
+
+	public void execute(MergeColumnOperationDescriptor operation)
+			throws SQLException {
+		(new AddColumnExecutor(this.getConnection())).execute(operation
+				.getDestinationColumn());
+
 		StringBuffer buf = new StringBuffer();
-		buf.append("UPDATE \"").append(operation.getTableName()).append("\" SET ")
+		buf.append("UPDATE \"").append(operation.getTableName())
+				.append("\" SET ")
 				.append(operation.getDestinationColumn().getTableName())
 				.append(" = ").append(operation.getSourceColumn1())
 				.append(" || '").append(operation.getDelimiter())
 				.append("' || ").append(operation.getSourceColumn2());
-		
+
 		Statement stmt = getConnection().createStatement();
-        stmt.executeUpdate(buf.toString());
-		
-        DropColumnExecutor dcx = new DropColumnExecutor(getConnection());
-        DropColumnOperationDescriptor dcod = new DropColumnOperationDescriptor(operation.getTableName(), operation.getSourceColumn1());
-        dcx.execute(dcod);
+		stmt.executeUpdate(buf.toString());
+
+		DropColumnExecutor dcx = new DropColumnExecutor(getConnection());
+
+		ITable table = new Table();
+		table.setName(operation.getTableName());
+		DropColumnOperationDescriptor dcod = new DropColumnOperationDescriptor(
+				table, operation.getSourceColumn1());
+		dcx.execute(dcod);
 	}
 
 }
