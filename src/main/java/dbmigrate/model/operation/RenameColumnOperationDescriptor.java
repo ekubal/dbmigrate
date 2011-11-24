4ca9720611ea267367f44f3d3c69386fd8db0b6d
Tomasz Kraus <tomasz@kraus.pl>
2011-11-24 18:06:34 +0100

    Dodanie egzekutora i deskryptora RenameColumn, test rename column refs #89

---
+++ src/main/java/dbmigrate/model/operation/RenameColumnOperationDescriptor.java
@@ -0,0 +1,40 @@
+package dbmigrate.model.operation;
+
+import dbmigrate.model.db.IColumn;
+import dbmigrate.model.db.ITable;
+
+public class RenameColumnOperationDescriptor implements IOperationDescriptor {
+	private IColumn oldColumn;
+	private IColumn newColumn;
+	private ITable table;
+	
+	public RenameColumnOperationDescriptor(ITable table, IColumn oldColumn,
+			IColumn newColumn) {
+		super();
+		this.oldColumn = oldColumn;
+		this.newColumn = newColumn;
+		this.table = table;
+	}
+	
+	public IColumn getOldColumn() {
+		return oldColumn;
+	}
+	public void setOldColumn(IColumn oldColumn) {
+		this.oldColumn = oldColumn;
+	}
+	public IColumn getNewColumn() {
+		return newColumn;
+	}
+	public void setNewColumn(IColumn newColumn) {
+		this.newColumn = newColumn;
+	}
+	public ITable getTable() {
+		return table;
+	}
+	public void setTable(ITable table) {
+		this.table = table;
+	}
+
+
+	
+}
