e95335edd4c0a962fc6251be33429186a660b6f1
Piotr Krawiec <pkrawiec@student.agh.edu.pl>
2011-12-01 17:59:21 +0100

    Fixed creation of migration configuration unit test.

--- src/test/java/dbmigrate/parser/LoaderTest.java
+++ src/test/java/dbmigrate/parser/LoaderTest.java
@@ -9,29 +9,32 @@ import dbmigrate.model.operation.MigrationConfiguration;
 import dbmigrate.parser.model.Migration;
 import dbmigrate.parser.model.RemoveColumn;
 
-public class LoaderTest  extends TestCase{
-	
-	public void testMapping() throws Exception {
-		Migration m=new Migration();
-		RemoveColumn rm=new RemoveColumn();
-		rm.setTable("TEST");
-		rm.setName("TEST2|");
-		
-		m.getDoList().add(rm);
-		Loader.map(m);
+public class LoaderTest extends TestCase {
+
+    public void testMapping() throws Exception {
+	Migration m = new Migration();
+	RemoveColumn rm = new RemoveColumn();
+	rm.setTable("TEST");
+	rm.setName("TEST2|");
+
+	m.getDoList().add(rm);
+	Loader.map(m);
+    }
+
+    public void testCreateMigrationConfiguration() {
+	try {
+	    MigrationConfiguration mc = Loader.load(new File(
+		    "migrations/2011111001_first_migration.xml"), false);
+	    CreateTableOperationDescriptor desc = (CreateTableOperationDescriptor) mc
+		    .getOperations().get(0);
+	    CreateTableExecutor executor = new CreateTableExecutor(null);
+	    assertEquals(
+		    "CREATE TABLE \"users\" ( id INT NOT NULL,username VARCHAR (40) NOT NULL,password VARCHAR (40) NOT NULL);",
+		    executor.createSql(desc).trim());
+	} catch (Exception e) {
+	    e.printStackTrace();
 	}
-	
-	public void testCreateMigrationConfiguration(){
-		try {
-			MigrationConfiguration mc = Loader.load(new File("migrations/2011111001_first_migration.xml"), false);
-			CreateTableOperationDescriptor desc = (CreateTableOperationDescriptor) mc.getOperations().get(0);
-			CreateTableExecutor executor = new CreateTableExecutor(null);
-			assertEquals("CREATE TABLE \"users\" ( id INT NOT NULL,username TEXT (40) NOT NULL,password TEXT (40) NOT NULL);", executor.createSql(desc).trim());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
+
+    }
 
 }
